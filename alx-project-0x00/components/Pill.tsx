7c7c8804f7c50f2956acfdd46206bbe9cb304687
johnajay17344 <akinsmarble123@gmail.com>
2025-09-18 18:15:01 +0100

    gitnode4

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,5 +1,9 @@
-// components/Pill.tsx
-const Pill = () => {
-  return <div></div>
-}
-export default Pill
+const Pill: React.FC = () => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
+    </div>
+  );
+};
+
+export default Pill;
