e638deb6412d3e8d89bfcc6f9eff10f9e23c000d
johnajay17344 <akinsmarble123@gmail.com>
2025-09-18 18:36:36 +0100

    nextjs setup

---
+++ alx-project-0x00/components/button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ButtonProps {
+  title: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  title,
+  size = "medium",
+  shape = "rounded-md",
+}) => {
+  // handle sizes
+  const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  return (
+    <button
+      className={`bg-blue-500 text-white ${sizeClasses[size]} ${shape} hover:bg-blue-600 transition`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,4 +1,5 @@
 // pages/landing.tsx
+import Button from "../components/Button";
 import Card from "@/components/Card";
 const Landing: React.FC = () => {
   return (
@@ -8,8 +9,11 @@ const Landing: React.FC = () => {
       <Card />
       font-extralight"
       >
-   git     Landing Page
+        git Landing Page
       </h1>
+      <Button title="Small Button" size="small" shape="rounded-sm" />
+      <Button title="Medium Button" size="medium" shape="rounded-md" />
+      <Button title="Large Button" size="large" shape="rounded-full" />
     </div>
   );
 };
