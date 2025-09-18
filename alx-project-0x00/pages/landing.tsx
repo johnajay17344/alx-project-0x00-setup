961523ae36563d5f00a498321c969061de0089de
johnajay17344 <akinsmarble123@gmail.com>
2025-09-18 18:21:07 +0100

    numver5

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,15 @@
 // pages/landing.tsx
-
+import Card from "@/components/Card";
 const Landing: React.FC = () => {
   return (
     <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <h1
+        className="text-xl 
+      <Card />
+      font-extralight"
+      >
+   git     Landing Page
+      </h1>
     </div>
   );
 };
